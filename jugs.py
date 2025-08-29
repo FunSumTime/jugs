e3aa0d229323f99886d32a3c4add815cca1305b1
FunSumTime <152440730+FunSumTime@users.noreply.github.com>
2025-08-29 16:55:35 -0600

    Delete jugs.py

--- jugs.py
+++
@@ -1,7 +0,0 @@
-class Node:
-    def __init__(self, state, parent=None, action=None, cost=0, cap):
-        self.state = state  # The current state of the jugs (e.g., a tuple: (2, 0))
-        self.parent = parent  # Reference to the node that created this one
-        self.cost = cost  # The number of steps to reach this state
-        self.capacity = cap
-
